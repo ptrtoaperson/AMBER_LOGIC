d85d233165e00063b1ed9cd3e7c245c85797a841
pointertoaperson <umesh0puri@gmail.com>
2026-08-10 16:03:56 +0300

    Save local changes

--- amber_controller/README.md
+++
@@ -1,3 +0,0 @@
-# amber_controller
-
-Package-local project file for editable installs from this folder.
--- amber_controller/__init__.py
+++
@@ -1,5 +0,0 @@
-"""SemiQon voltage-source Python package."""
-
-from .client import controller
-
-__all__ = ["controller"]
